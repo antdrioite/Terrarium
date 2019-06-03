4cfb1aa26f0a097ac0f119aee5c8617176c4266b
[antdrioite] <[cnudde1@live.be]>
2019-06-03 13:21:46 +0200

    start main

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -1,8 +1,19 @@
 package be.vdab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        private static final Scanner scanner = new Scanner(Sy)
+        //maak een terrarium en gamecontroller object
+	    Terrarium terrarium = new Terrarium();
+	    GameController controller = new GameController();
+	    //print dag 1
+	    controller.print();
+	    //vraag en check input aan de user
+	    try (
+
+            )
     }
 }

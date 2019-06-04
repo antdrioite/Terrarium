5f729ca0e832c4ff4d0196b03c0a01b5c9ded025
[antdrioite] <[cnudde1@live.be]>
2019-06-04 09:46:53 +0200

    update naar maven, web en spring

---
+++ src/main/java/be/vdab/domain/Terrarium.java
@@ -0,0 +1,80 @@
+package be.vdab.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Terrarium {
+	
+	List<Organism> grid;
+	
+	public void addOrganism(Organism organism) {
+		grid.add(organism);
+	
+	}
+	
+	
+	
+	//public Location getXemptyLocations() {
+	//	
+	//}
+	
+	
+	
+	public Terrarium() {
+		
+		grid = new ArrayList<>();
+		Location een = new Location(0,0);
+		Location twee = new Location(0,1);
+		Location drie = new Location(0,3);
+		Location vier = new Location(0,4);
+		Location vijf = new Location(3,0);
+		Location zes = new Location(3,1);
+		
+		grid.add(new Plant(twee, 1));
+		grid.add(new Herbivore(een, 0));
+		grid.add(new Herbivore(drie, 0));
+		grid.add(new Herbivore(vier, 0));
+		grid.add(new Herbivore(zes, 0));
+		grid.add(new Carnivore(vijf, 0));
+		
+	}
+
+
+
+	public List<Location> getEmptyLocations() {
+		
+		List<Location> legePlekken = new ArrayList<>();
+		
+		for (Organism organism : grid) {
+			if (!(organism instanceof Organism)) {
+				legePlekken.add(organism.getLocation());	
+			}
+		}
+		return legePlekken;
+	}
+	
+	public Terrarium(List<Organism> grid) {
+		super();
+		this.grid = grid;
+	}
+
+	public List<Organism> getAllOrganisms() {
+		
+		List<Organism> alleOrganisms = new ArrayList<>();
+		
+		for(Organism organism : grid) {
+			if (organism instanceof Organism) {
+				alleOrganisms.add(organism);
+			}	
+		}
+		return alleOrganisms;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}

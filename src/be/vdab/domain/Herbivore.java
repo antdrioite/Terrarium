5417a1bfafc8df4bf99085081cd46bfba96d80a2
AVHoecke <49674824+AVHoecke@users.noreply.github.com>
2019-06-04 13:12:07 +0200

    Herbivore interactWithEnvironment method update

--- src/be/vdab/domain/Herbivore.java
+++ src/be/vdab/domain/Herbivore.java
@@ -1,6 +1,8 @@
 package be.vdab.domain;
 
-public class Herbivore extends Animal{
+import java.util.List;
+
+public class Herbivore extends Animal {
 
     public Herbivore(Location location, int lifeForce, Terrarium terrarium) {
         super(location, lifeForce, terrarium);
@@ -8,6 +10,27 @@ public class Herbivore extends Animal{
 
     @Override
     public boolean interactWithEnvironment() {
-        return true;
+        Organism neighbourOrganism = null;
+        List<Organism> organisms = this.getTerrarium().getAllOrganisms();
+        Location organismToMyRight = new Location(getLocation().getX() + 1, getLocation().getY());
+        for (Organism organism : organisms) {
+            if (organism.getLocation().equals(organismToMyRight)) {
+                neighbourOrganism = organism;
+            }
+        }
+        switch (neighbourOrganism) {
+            case neighbourOrganism.getClass().equals(Plant.class):
+                this.setLifeForce(this.getLifeForce() + neighbourOrganism.getLifeForce());
+                this.getTerrarium().remove(neighbourOrganism);
+                return true;
+            case neighbourOrganism.getClass().equals(Herbivore.class):
+                System.out.println("Love is in the air!");
+                Terrarium.addNewHerbivore;
+                return true;
+            case neighbourOrganism == null:
+                return this.move();
+            default:
+                return false;
+        }
     }
 }

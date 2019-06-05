666d79395e53b2edef46e79dc71263565ab68213
antdrioite <47104104+antdrioite@users.noreply.github.com>
2019-06-05 13:20:30 +0200

    move aangepast, geen endless loops
    dieren lopen door muren, bewegen niet als ze vastzitten

--- src/main/java/be/vdab/domain/Animal.java
+++ src/main/java/be/vdab/domain/Animal.java
@@ -14,32 +14,32 @@ public abstract class Animal extends Organism {
     }
 
     public boolean move() {
-        boolean foundEmptyLocation = false;
+
         int currentX = this.getLocation().getX();
         int currentY = this.getLocation().getY();
-        while (!foundEmptyLocation) {
-            int newX = (int) (Math.random() * (((currentX + 1) - (currentX - 1)) + 1)) + (currentX - 1);
-            int newY = currentY;
-            while ((newX == currentX) && (newY == currentY)) {
-                newY = (int) (Math.random() * (((currentY + 1) - (currentY - 1)) + 1)) + (currentY - 1);
-            }
-            if (newX > 5) {
-                newX = 0;
-            }
-            if (newX < 0) newX = 5;
-            if (newY > 5) {
-                newY = 0;
-            }
-            if (newY < 0) new Y=5;
-            Location newLocation = new Location(newX, newY);
-            List<Location> emptyLocations = terrarium.getEmptyLocations();
-            for (Location emptyLocation : emptyLocations) {
-                if (emptyLocation.equals(newLocation) && !(newLocation.equals(this.getLocation()))) {
-                    this.setLocation(newLocation);
-                    foundEmptyLocation = true;
-                }
+
+        List<Location> emptyLocations = terrarium.getEmptyLocations();
+        List<Location> possibleLocation = new ArrayList<>();
+        possibleLocation.add(new Location(currentX +1, currentY));
+        possibleLocation.add(new Location(currentX -1, currentY));
+        possibleLocation.add(new Location(currentX , currentY +1));
+        possibleLocation.add(new Location(currentX , currentY -1));
+        for (Location location : possibleLocation) {
+            if(location.getX() > 5) location.setX(0);
+            if(location.getX() < 0) location.setX(5);
+            if(location.getY() > 5) location.setY(0);
+            if(location.getY() < 0) location.setY(5);
+        }
+        List<Location> removeList = new ArrayList<>();
+        for (Location location : possibleLocation) {
+            if(!emptyLocations.contains(location)) {
+                removeList.add(location);
             }
         }
+        possibleLocation.removeAll(removeList);
+        if (possibleLocation.isEmpty()) return false;
+        int randomNumber = (int) Math.ceil(Math.random() * possibleLocation.size())-1;
+        this.setLocation(possibleLocation.get(randomNumber));
         return true;
     }
 
